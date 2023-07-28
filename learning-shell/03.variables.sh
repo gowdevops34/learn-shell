4e15cbc63fca710cc17c15fdffa67f4f120dcaf1
gowthami <kgowthami34@gmail.com>
2023-07-28 14:57:33 +0530

    variables demo

---
+++ learning-shell/03.variables.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+# Declare variable
+a=100
+
+# Print data in variable
+echo $a
+#or
+echo ${a}
+
+# Arithmetic Substitution
+b=$((3+5))
+echo $b
+
+# Command Substitution
+DATE=$(date +%F)
+
+echo Welcome, Today date is $DATE
+
+# variables from command line
+#echo c=$c
